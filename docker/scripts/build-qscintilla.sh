349968d319ccceeadda66792c1a8469036ea3c7a
Matthias Kuhn <matthias@opengis.ch>
2016-03-10 19:29:55 +0100

    Add -qt5 suffix to QWT as well
    In a quite ugly manner

--- docker/scripts/build-qscintilla.sh
+++ docker/scripts/build-qscintilla.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export C_INCLUDE_PATH=/usr/include/python3.2mu/
 export CPLUS_INCLUDE_PATH=/usr/include/python3.2mu/
 
-# wget http://sourceforge.net/projects/pyqt/files/QScintilla2/QScintilla-2.9.1/QScintilla-gpl-2.9.1.tar.gz
+wget http://sourceforge.net/projects/pyqt/files/QScintilla2/QScintilla-2.9.1/QScintilla-gpl-2.9.1.tar.gz
 tar xzf QScintilla-gpl-2.9.1.tar.gz
 pushd QScintilla-gpl-2.9.1
 patch -p1 < ${DIR}/patches/qscintilla.patch
--- docker/scripts/build-qwt.sh
+++ docker/scripts/build-qwt.sh
@@ -9,4 +9,13 @@ sed -i "s|QWT_INSTALL_PREFIX *=.*|QWT_INSTALL_PREFIX = ${PREFIX}|g" qwtconfig.pr
 qmake
 make -j${CORES} > /dev/null
 make install > /dev/null
+pushd ${PREFIX}/lib
+mv libqwt.so.6.1.2 libqwt-qt5.so.6.1.2
+rm libqwt-qt5.so
+rm libqwt-qt5.so.6
+rm libqwt-qt5.so.6.1
+ln -s libqwt-qt5.so.6.1.2 libqwt-qt5.so.6.1
+ln -s libqwt-qt5.so.6.1.2 libqwt-qt5.so.6  
+ln -s libqwt-qt5.so.6.1.2 libqwt-qt5.so  
+popd
 popd
