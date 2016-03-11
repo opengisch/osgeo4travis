d7d708e8f27cd22f6580ca0d8c892beae21ed6ba
Matthias Kuhn <matthias@opengis.ch>
2016-03-11 08:40:51 +0100

    Proper suffixing for qwt

--- docker/scripts/build-qwt.sh
+++ docker/scripts/build-qwt.sh
@@ -5,17 +5,9 @@ wget http://downloads.sourceforge.net/qwt/qwt/6.1.2/qwt-6.1.2.tar.bz2
 
 tar -xjf qwt-6.1.2.tar.bz2
 pushd qwt-6.1.2
+patch -p1 < ${DIR}/patches/qwt.patch
 sed -i "s|QWT_INSTALL_PREFIX *=.*|QWT_INSTALL_PREFIX = ${PREFIX}|g" qwtconfig.pri
 qmake
 make -j${CORES} > /dev/null
 make install > /dev/null
-pushd ${PREFIX}/lib
-mv libqwt.so.6.1.2 libqwt-qt5.so.6.1.2
-rm libqwt-qt5.so
-rm libqwt-qt5.so.6
-rm libqwt-qt5.so.6.1
-ln -s libqwt-qt5.so.6.1.2 libqwt-qt5.so.6.1
-ln -s libqwt-qt5.so.6.1.2 libqwt-qt5.so.6  
-ln -s libqwt-qt5.so.6.1.2 libqwt-qt5.so  
-popd
 popd
