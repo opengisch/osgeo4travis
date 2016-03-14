3b442cb5e092a241c972312a69ba858e8a4418e4
Matthias Kuhn <matthias@opengis.ch>
2016-03-14 13:53:55 +0100

    Fix qwt suffix

--- docker/scripts/build-qwt.sh
+++ docker/scripts/build-qwt.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Build qwt
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 wget http://downloads.sourceforge.net/qwt/qwt/6.1.2/qwt-6.1.2.tar.bz2
 
 tar -xjf qwt-6.1.2.tar.bz2
