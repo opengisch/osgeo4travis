1d57eafb45839144870996076c3ee0189504184c
Matthias Kuhn <matthias@opengis.ch>
2016-04-08 12:15:23 +0200

    Build lighttpd

---
+++ docker/qt4/scripts/build-lighttpd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Build spawn-fcgi
+wget http://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.39.tar.gz
+tar lighttpd-1.4.39.tar.gz
+mkdir build-lighttpd
+pushd build-lighttpd
+cmake \
+  -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
+  ../lighttpd-1.4.39
+make -j${CORES} > /dev/null
+make install > /dev/null
+popd
