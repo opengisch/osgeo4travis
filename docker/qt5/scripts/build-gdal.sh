f24993db99752398b0fd2b5733d4587084b23593
Matthias Kuhn <matthias@opengis.ch>
2016-07-02 12:07:02 +0200

    Build gdal with spatialite support

--- docker/qt5/scripts/build-gdal.sh
+++ docker/qt5/scripts/build-gdal.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Build gdal
+
 wget https://github.com/OSGeo/gdal/archive/trunk.tar.gz
 tar xvf trunk.tar.gz > /dev/null
 
@@ -7,7 +8,9 @@ pushd gdal-trunk/gdal
 ./configure --prefix=${PREFIX} \
   --with-python=python3 \
   --with-geos=${PREFIX}/bin/geos-config \
-  --without-libtool
+  --without-libtool \
+  --with-spatialite=dlopen \
+  --with-spatialite-soname=libspatialite.so
 make -j${CORES}
 make install > /dev/null
 popd
