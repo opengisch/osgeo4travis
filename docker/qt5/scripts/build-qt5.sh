62e4170d4c418e6cb8e17d9724b3921c25401454
Matthias Kuhn <matthias@opengis.ch>
2016-10-19 09:31:19 +0200

    Qt version script

--- docker/qt5/scripts/build-qt5.sh
+++ docker/qt5/scripts/build-qt5.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-wget http://download.qt.io/official_releases/qt/5.7/5.7.0/single/qt-everywhere-opensource-src-5.7.0.tar.gz
-tar -xvf qt-everywhere-opensource-src-5.7.0.tar.gz
-pushd qt-everywhere-opensource-src-5.7.0
+
+VERSION_qt5=5.7.0
+
+wget http://download.qt.io/official_releases/qt/5.7/${VERSION_qt5}/single/qt-everywhere-opensource-src-${VERSION_qt5}.tar.gz
+tar -xvf qt-everywhere-opensource-src-${VERSION_qt5}.tar.gz
+pushd qt-everywhere-opensource-src-${VERSION_qt5}
 ./configure --opensource --confirm-license=yes --platform=linux-clang --prefix=${PREFIX} -qt-xcb
 make -j${CORES}
 make install
