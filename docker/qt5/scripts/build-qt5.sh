025ab272a03fdcfb27c1d53b0644898de519b520
Matthias Kuhn <matthias@opengis.ch>
2016-11-09 13:25:19 +0100

    Qt 5.8

--- docker/qt5/scripts/build-qt5.sh
+++ docker/qt5/scripts/build-qt5.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-VERSION_qt5=5.7.0
+VERSION_qt5=5.8
+VERSION_qt5_complete=5.8.0-beta
 
-wget http://download.qt.io/official_releases/qt/5.7/${VERSION_qt5}/single/qt-everywhere-opensource-src-${VERSION_qt5}.tar.gz
-tar -xvf qt-everywhere-opensource-src-${VERSION_qt5}.tar.gz
-pushd qt-everywhere-opensource-src-${VERSION_qt5}
-./configure --opensource --confirm-license=yes --platform=linux-clang --prefix=${PREFIX} -qt-xcb
+#wget http://download.qt.io/official_releases/qt/5.7/${VERSION_qt5}/single/qt-everywhere-opensource-src-${VERSION_qt5}.tar.gz
+wget http://download.qt.io/development_releases/qt/${VERSION_qt5}/${VERSION_qt5_complete}/single/qt-everywhere-opensource-src-${VERSION_qt5_complete}.tar.gz
+tar -xvf qt-everywhere-opensource-src-${VERSION_qt5_complete}.tar.gz
+pushd qt-everywhere-opensource-src-${VERSION_qt5_complete}
+./configure -confirm-license -opensource --platform=linux-clang --prefix=${PREFIX} -qt-xcb
 make -j${CORES}
 make install
 popd  
