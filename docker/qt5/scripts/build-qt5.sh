84745295f3b3972e20c818db8f4e0194c9ae0e97
Matthias Kuhn <matthias@opengis.ch>
2016-11-15 08:15:13 +0100

    Qt 5.8 beta

--- docker/qt5/scripts/build-qt5.sh
+++ docker/qt5/scripts/build-qt5.sh
@@ -7,7 +7,8 @@ VERSION_qt5_complete=5.8.0-beta
 wget http://download.qt.io/development_releases/qt/${VERSION_qt5}/${VERSION_qt5_complete}/single/qt-everywhere-opensource-src-${VERSION_qt5_complete}.tar.gz
 tar -xvf qt-everywhere-opensource-src-${VERSION_qt5_complete}.tar.gz
 pushd qt-everywhere-opensource-src-${VERSION_qt5_complete}
-./configure -confirm-license -opensource --platform=linux-clang --prefix=${PREFIX} -qt-xcb
+./configure -confirm-license -opensource -platform linux-clang -prefix ${PREFIX} -qt-xcb
 make -j${CORES}
 make install
 popd  
+
